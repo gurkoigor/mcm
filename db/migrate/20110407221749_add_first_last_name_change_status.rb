8374482d0f0017ce38e3f25765235e22ca8dff81
Igor <gurko.igor@gmail.com>
2011-04-19 22:32:58 +0300

    merge firt and last name to full name

--- db/migrate/20110407221749_add_first_last_name_change_status.rb
+++ db/migrate/20110407221749_add_first_last_name_change_status.rb
@@ -1,13 +1,11 @@
 class AddFirstLastNameChangeStatus < ActiveRecord::Migration
   def self.up
-    add_column(:users, :first_name, :string)
-    add_column(:users, :last_name, :string)
+    add_column(:users, :full_name, :string)
     add_column(:users, :active, :boolean, :default => false)
   end
 
   def self.down
-    remove_column(:users, :first_name)
-    remove_column(:users, :last_name)
+    remove_column(:users, :full_name)
     remove_column(:users, :active)
   end
 end
