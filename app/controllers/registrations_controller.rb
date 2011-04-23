17f8ab8c87f5dee16ac2e972df9c33e6190345dc
Igor <gurko.igor@gmail.com>
2011-04-23 11:23:07 +0300

    fixed return flash messages

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,21 +9,21 @@ class RegistrationsController < Devise::RegistrationsController
     @user = User.new(params[:user])
     if @parent_user.nil?
       @user.errors.add(:parent_id)
-      flash[:alert] = "Реферер не найден"
+      flash.now[:alert] = "Реферер не найден"
       render :action => :new
     else
       if @user.save
-        flash[:notice] = "Вы успешно зарегестрированы в сети"
+        flash.now[:notice] = "Вы успешно зарегестрированы в сети"
         sign_in_and_redirect(resource_name, @user)
       else
-        flash[:alert] = "Вы не зарегестрированы"
+        flash.now[:alert] = "Вы не зарегестрированы"
         render :action => :new
       end
     end
   end
 
   def destroy
-    flash[:error] = "Опция не доступна!!!"
+    flash.now[:error] = "Опция не доступна!!!"
     redirect_to root_path
     #Метод для удаления самого себя, для востановления использовать super или убрать метод с контроллера
   end
